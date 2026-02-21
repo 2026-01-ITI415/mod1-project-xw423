61b1feb76cecaa8831a0bac30679c6a3138c91a1
Myusuki <myusuki_86666@tutanota.com>
2026-02-21 10:31:06 -0500

    Setting things up for later in the Game Master script

--- Assets/Scripts/Riddler.cs
+++ Assets/Scripts/Riddler.cs
@@ -8,13 +8,44 @@ public class Riddler : MonoBehaviour
 	public Text narrator;
 	public Text strikes;
 	public int strikesLeft = 3;
+	public bool gameOver = false;
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
 	{
 		narrator.text = "Welcome, to Simply Riddle Me!";
 		strikes.text = "Strikes: " + strikesLeft;
+		Win();
 	}
+
+	// intro
+	void Intro()
+	{
+		narrator.text = "Are you ready to be riddled?";
+		// yes
+		// no
+	}
+	// riddles
+	void Riddle1()
+	{}
+	void Riddle2()
+	{}
+	void Riddle3()
+	{}
+	// end game screens
+	void Win()
+	{
+		narrator.text = "Congratulations! You've solved all riddles with " + strikesLeft + " strikes left.";
+	}
+	void GameOver()
+	{
+		narrator.text = "Looks like you need to brush up on your riddles.\nBetter luck next time!";
+	}
+
+	void But1()
+	{}
+	void But2()
+	{}
 
 	// Update is called once per frame
 	void Update()
